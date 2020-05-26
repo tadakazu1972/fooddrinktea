e5b3cbb53ff09e44454202bb229b5870dc482fab
osakacityict <ictosakacity@gmail.com>
2020-05-26 12:39:07 +0900

    描画

--- 住所緯度経度集計.R
+++ 住所緯度経度集計.R
@@ -1,4 +1,9 @@
+#ライブラリ読込
 library(dplyr)
+library(sf)
+library(readr)
+library(RColorBrewer)
+library(classInt)
 
 setwd("~/Desktop/飲食店喫茶店")
 
@@ -15,40 +20,16 @@ dataA <- dataA %>% rename(緯度=Group.1, 経度=Group.2, 店舗数=x)
 #書き出し
 write.csv(dataA, "飲食店喫茶店緯度経度別集計.csv", fileEncoding="CP932", row.names=FALSE)
 
+###################################################
+#　24区地図描画
+#地図ファイル読込
+shape <- st_read("h27_did_27.shp") #eStatから大阪府を取得
 
-#描画
-{
-  #書き出しファイル設定
-  quartz(type="pdf", file="クラス別平均_H28_5年4教科.pdf")
+#フォント
+par(family="HiraKakuProN-W3")
 
-  #描画
-  par(family="HiraKakuProN-W3")
+#区境 xlimとylimをshapeと合わせないとずれる
+plot(st_geometry(shape[1:24,4]), xlim=c(135.4,135.6), ylim=c(34.59,34.77), col="white", main="食品営業許可施設（飲食店、喫茶店）　令和元年12月27日現在")
 
-  #線形回帰計算
-  result <- lm(標準化得点4教科~児童数, dataC)
-
-  #相関係数　タイトルに使うので先に計算
-  r <- cor(dataC$児童数, dataC$標準化得点4教科)
-
-  #学校
-  plot(result$model$児童数, result$model$標準化得点4教科, pch=16, col=4, cex=1, xlab="クラス別　平成28年　5年　児童数", ylab="クラス別　平成28年 5年　4教科合計(標準化得点)", main=paste("クラス別　標準化得点と児童数の相関  (r=", round(r, digits=2), ")", sep=""))
-  text(result$model$児童数, result$model$標準化得点4教科+0.4, labels=dataC$学校名, cex=0.4)
-
-  #回帰直線
-  abline(result, lwd=3)
-
-  # 回帰直線の傾きと切片をそれぞれa,bに代入
-  a <- result$coefficients[["児童数"]]
-  b <- result$coefficients[["(Intercept)"]]
-  plus <- ifelse(b>0, "+", "") #bが以上であれば"+"の記号を描画。マイナスであればNull
-
-  #R2を取り出す
-  name <- summary(result)
-  r2 <- name$r.squared
-
-  #回帰式とR2　描画
-  legend("topright", legend=paste("y=", round(a, digits=4), "x ", plus, " ", round(b, digits=4), sep=""))
-  legend("bottomleft", legend=paste("R^2 =", round(r2, digits=4), sep=""))
-
-  dev.off()
-}
+#店舗をプロット(円の大きさは店舗数)
+points(dataA$経度, dataA$緯度, pch=16, col="red", cex=0.3)
